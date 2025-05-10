062cf76e2ee467b4fb6d926ecfac841f5d7b90e8
Tiago Leite <tiagobscleite@gmail.com>
2025-05-10 12:24:31 +0100

    ex00 feito e inicio do ex01

---
+++ cpp00/ex00/src/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << '\n';
+	return (0);
+}
