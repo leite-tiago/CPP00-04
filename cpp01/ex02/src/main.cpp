6260cf63e043250d38f5206e9e1b6e4f978a5271
Tiago Leite <tiagobscleite@gmail.com>
2025-05-12 16:08:23 +0100

    cpp01-ex02 done

---
+++ cpp01/ex02/src/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tborges- <tborges-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 15:47:00 by tborges-          #+#    #+#             */
+/*   Updated: 2025/05/12 16:06:53 by tborges-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "memory address of the string variable: " << &str << std::endl;
+	std::cout << "memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "value of the string variable: " << str << std::endl;
+	std::cout << "value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value pointed to by stringREF: " << stringREF << std::endl;
+}
