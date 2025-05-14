895d317a417d542069879a5d88eb5e4cfe50865c
Tiago Leite <tiagobscleite@gmail.com>
2025-05-14 16:31:13 +0100

    cpp03-ex01 done

--- cpp03/ex01/inc/ScavTrap.hpp
+++ cpp03/ex01/inc/ScavTrap.hpp
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tborges- <tborges-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/14 12:03:44 by tborges-          #+#    #+#             */
-/*   Updated: 2025/05/14 16:27:51 by tborges-         ###   ########.fr       */
+/*   Created: 2025/05/14 15:28:16 by tborges-          #+#    #+#             */
+/*   Updated: 2025/05/14 15:33:57 by tborges-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/ClapTrap.hpp"
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+#include "ClapTrap.hpp"
+#include <iostream>
 
-int	main(void)
+class ScavTrap : public ClapTrap
 {
-	ClapTrap bot("Clappy");
-	
-	bot.attack("Target Dummy");
-	bot.takeDamage(4);
-	bot.beRepaired(3);
-	bot.takeDamage(10);
-	bot.attack("Another Target");
-	bot.beRepaired(5);
+	public:
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap& other);
+		ScavTrap& operator=(const ScavTrap& other);
+		~ScavTrap();
 
-	return (0);
-}
+		void	attack(const std::string& target);
+		void	guardGate();
+};
+
+#endif
--- cpp03/ex01/src/main.cpp
+++ cpp03/ex01/src/main.cpp
@@ -6,22 +6,26 @@
 /*   By: tborges- <tborges-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 12:03:44 by tborges-          #+#    #+#             */
-/*   Updated: 2025/05/14 16:27:51 by tborges-         ###   ########.fr       */
+/*   Updated: 2025/05/14 16:30:07 by tborges-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/ClapTrap.hpp"
+#include "../inc/ScavTrap.hpp"
 
-int	main(void)
-{
-	ClapTrap bot("Clappy");
-	
-	bot.attack("Target Dummy");
-	bot.takeDamage(4);
-	bot.beRepaired(3);
-	bot.takeDamage(10);
-	bot.attack("Another Target");
-	bot.beRepaired(5);
+int main() {
+    std::cout << "---- Creating ClapTrap ----" << std::endl;
+    ClapTrap a("CL4P-TP");
+    a.attack("Bandit");
+    a.beRepaired(5);
+    a.takeDamage(6);
 
-	return (0);
+    std::cout << "\n---- Creating ScavTrap ----" << std::endl;
+    ScavTrap s("SC4V-TP");
+    s.attack("Raid Boss");
+    s.beRepaired(10);
+    s.takeDamage(40);
+    s.guardGate();
+
+    std::cout << "\n---- Destruction ----" << std::endl;
+    return 0;
 }
